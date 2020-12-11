d84b8adaaf4bcb1cc8fbb7af1a510dd91bca7e12
Maxtasy <maxtasy88@web.de>
2020-12-11 22:11:04 +0100

    Add day 20 solution

---
+++ day21-2.py
@@ -0,0 +1,30 @@
+# https://adventofcode.com/2015/day/20
+
+
+from collections import defaultdict
+
+
+UPPER_BOUND = 1000000
+
+
+def part1(input_file):
+  with open(input_file, "r") as f:
+    target = int(f.read().strip())
+    
+    houses = defaultdict(int)
+
+    for elf in range(1, target):
+      for house in range(elf, min(elf*50+1, UPPER_BOUND), elf):
+        houses[house] += elf * 11
+      
+      if houses[elf] >= target:
+        return elf
+      
+
+def main():
+	input_file = "day20-input.txt"
+	print(part1(input_file))
+
+
+if __name__ == "__main__":
+	main()
